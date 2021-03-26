7f103896e7137b384f4080a9a1fb66bc0b147271
Pieter Krige <pkrige@PJK-IMAC.local>
2021-03-26 19:04:10 -0400

    memory implemented

--- a3_2.h
+++ a3_2.h
@@ -1,10 +1,13 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
+#include <time.h>
 
 typedef struct process{
     char p_id;
     int req_mem;
+    time_t time_added;
+    int swapped;
 } process;
 
 typedef struct my_queue{
@@ -13,7 +16,23 @@ typedef struct my_queue{
     process* processes;
 } my_queue;
 
+typedef struct memory{
+    process mem[128];
+
+    int next_element;
+
+    int size;
+    int capacity;
+
+    //stats variables
+    double usage;
+    double average_util;
+} memory;
+
 process* readfile(char* filename);
-my_queue* makeQueue();
+my_queue* makeQueue(process* to_add);
 process dequeue(my_queue* queue);
 void enqueue(my_queue *queue, process to_add);
+void free_structs(my_queue* queue);
+void mem_store(memory* my_mem, process to_store);
+void mem_swap(memory* my_mem,process to_swap);
--- memory.c
+++ memory.c
@@ -1,17 +1,31 @@
 #include "a3_2.h"
 
+my_queue *queue;
+
 int main(int argc, char* argv[]){
     if(argc < 3){
         fprintf(stderr,"INSUFFICIENT ARGUMENTS: Please enter the filename followed by the insertion style. ex. ./holes filename.txt first/next/best/worst\n");
     } else{
+        memory* mem = calloc(1, sizeof(memory));
+        mem->capacity = 128;
+
+       //printf("%p is equal to %p",(void*) mem->mem, (void*) mem->last_element);
+
         process* to_process = readfile(argv[1]);
-        my_queue *queue = makeQueue(to_process);
+        queue = makeQueue(to_process);
 
         process temp = dequeue(queue);
-
         printf("Popped of the queue: pid: %c %d\n", temp.p_id, temp.req_mem);
+        mem_store(mem,temp);
+        
+        temp = dequeue(queue);
+        printf("Popped of the queue: pid: %c %d\n", temp.p_id, temp.req_mem);
+        mem_store(mem,temp);
 
-        enqueue(queue,temp);
+        temp = dequeue(queue);
+        printf("Popped of the queue: pid: %c %d\n", temp.p_id, temp.req_mem);
+        mem_store(mem,temp);
+
 
         if(strcmp("first", argv[2])== 0){
             printf("first\n");
@@ -25,6 +39,7 @@ int main(int argc, char* argv[]){
             printf("Please enter either first, next, best, or worst. Exiting now... Goodbye\n");
             exit(1);
         }
+        free_structs(queue);
     }
     return 0;
 }
@@ -34,7 +49,7 @@ process* readfile(char* filename){
     char r_char;
     int r_int;
     int i = 0;
-    process *processes = calloc(50,sizeof(process));
+    process *processes = calloc(150,sizeof(process));
 
     if(fp == NULL){
         fprintf(stderr,"Could not open file %s\n",filename);
@@ -47,7 +62,7 @@ process* readfile(char* filename){
         i++;
     }
 
-    processes = realloc(processes,i*sizeof(process));
+    //processes = realloc(processes,i*sizeof(process));
 
 /*     for(int j = 0; j < i; j++){
         printf("Process ID: %c, Required Memory: %d\n",processes[j].p_id,processes[j].req_mem);
@@ -65,7 +80,7 @@ my_queue* makeQueue(process* to_add){
 
     queue->processes = to_add;
 
-    printf("Suxxessfully initialized the queue.\n");
+    printf("Successfully initialized the queue.\n");
     return queue;
 }
 
@@ -77,9 +92,9 @@ void enqueue(my_queue *queue, process to_add){
 }
 
 process dequeue(my_queue* queue){
-    if(queue->size == 0){
-        printf("Nothign to remove, queue empty.\n");
-    }
+/*     if(queue->size == 0){
+        printf("Nothing to remove, queue empty.\n");
+    } */
 
     process temp = queue -> processes[queue->start];
 
@@ -89,3 +104,70 @@ process dequeue(my_queue* queue){
     return temp;
 }
 
+void free_structs(my_queue* queue){
+    free(queue->processes);
+    free(queue);
+}
+
+void mem_store(memory* my_mem, process to_store){
+    printf("Process to add %c %d\n", to_store.p_id, to_store.req_mem);
+    if((my_mem->size + to_store.req_mem) >= my_mem->capacity){
+        printf("Swap with oldest\n");
+        mem_swap(my_mem,to_store);
+    } else {
+        to_store.time_added = time(0);
+
+        for(int i = 0; i < to_store.req_mem; i++){
+            my_mem->mem[my_mem->next_element++] = to_store;
+        }
+
+        my_mem->size += to_store.req_mem;
+        //printf("\t\t%d\n\r", my_mem->size);
+    }
+    
+/*     for(int j = 0; j < my_mem->size; j++){
+        printf("%c %d\n",my_mem->mem[j].p_id, my_mem->mem[j].req_mem);
+    }  */
+}
+
+void mem_swap(memory* my_mem,process to_swap){
+    int loc = 0;
+
+    for(int i = 0; i < my_mem->capacity; i++){
+       //printf("Time 1: %ld Time 2: %ld\n", my_mem->mem[i].time_added, my_mem->mem[loc].time_added);
+        if((my_mem->mem[i].time_added < my_mem->mem[loc].time_added) && (my_mem->size - my_mem->mem[i].req_mem >= to_swap.req_mem)){
+            if(my_mem->mem[i].time_added == 0){
+                continue;
+            }
+            loc = i;
+        }
+    }
+
+    process temp = my_mem->mem[loc];
+    temp.swapped++;
+    
+    my_mem->size -= temp.req_mem;
+
+    for(int j = loc; j < temp.req_mem; j++){
+        my_mem->mem[j].p_id = 0;
+        my_mem->mem[j].req_mem = 0;
+        my_mem->mem[j].time_added = 0;
+        my_mem->mem[j].swapped = 0;
+    }
+
+    to_swap.time_added = time(0);
+
+    for(int m = loc; m < loc + to_swap.req_mem; m++){
+        my_mem->mem[m] = to_swap;
+    }
+    
+/*     for(int k = 0; k < my_mem->capacity; k++){
+        printf("%c %d\n",my_mem->mem[k].p_id, my_mem->mem[k].req_mem);
+    } */
+    
+    my_mem->size += to_swap.req_mem;
+    
+    printf(" Location of oldest: %d\n", loc);
+
+    enqueue(queue,temp);
+}
